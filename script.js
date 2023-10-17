7979aa97b72eb6ace5edb30fe266e0b13f893f1a
sumanyaselvi-r <145419732+sumanyaselvi-r@users.noreply.github.com>
2023-10-17 18:27:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let timer=10;
+let time=document.getElementById("Display");
+setTimeout(()=>{
+    time.innerHTML=timer--;
+    setTimeout(()=>{
+        time.innerHTML=timer--;
+        setTimeout(()=>{
+            time.innerHTML=timer--;
+            setTimeout(()=>{
+                time.innerHTML=timer--;
+                setTimeout(()=>{
+                    time.innerHTML=timer--;
+                    setTimeout(()=>{
+                        time.innerHTML=timer--;
+                        setTimeout(()=>{
+                            time.innerHTML=timer--;
+                            setTimeout(()=>{
+                                time.innerHTML=timer--;
+                                setTimeout(()=>{
+                                    time.innerHTML=timer--;
+                                    setTimeout(()=>{
+                                        time.innerHTML=timer--;
+                                        setTimeout(()=>{
+                                            time.innerHTML="Happy Independence Day";
+                                            
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                    
+                    },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000)
